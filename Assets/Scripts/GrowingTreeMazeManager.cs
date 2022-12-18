850051f24f5943abce95eccc7e291d6ca11cebb2
niguelchaos <niguelchaos@gmail.com>
2022-12-18 15:10:40 +0100

    added oldest as option, cleaned up more code

--- Assets/Scripts/GrowingTreeMazeManager.cs
+++ Assets/Scripts/GrowingTreeMazeManager.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 public class GrowingTreeMazeManager : MonoBehaviour
 {
+    public enum ACTIVE_SELECTION_METHOD { RANDOM, NEWEST, OLDEST}
+
     private Dictionary<int, Vector2> DIRECTIONS = new Dictionary<int, Vector2>
     {
         { 0, new Vector2(0, 1) }, // up
@@ -15,8 +17,8 @@ public class GrowingTreeMazeManager : MonoBehaviour
         { 3, new Vector2(-1, 0) } // right
     }; 
     
-    [SerializeField] bool useRandomActiveNode = false;
 
+    [SerializeField] private ACTIVE_SELECTION_METHOD cellSelectMethod = ACTIVE_SELECTION_METHOD.RANDOM;
     [SerializeField] bool useSteps;
     [SerializeField] float stepWaitTime = 0.03f;
     [SerializeField] int steps;
@@ -50,11 +52,8 @@ public class GrowingTreeMazeManager : MonoBehaviour
     {
         if (!useSteps || (useSteps && currentSteps < steps))
         {
-            
             GenerateMaze();
-            currentSteps++;
         }
-        StartCoroutine("Wait");
     }
 
     private void GenerateMaze()
@@ -62,66 +61,11 @@ public class GrowingTreeMazeManager : MonoBehaviour
         if (this.path.Count != 0 && !finishedMaze)
         {
             // Choose "active" node to check neighbors
-            MazeNode currentNode;
-
-            if (useRandomActiveNode)
-            {
-                // Select random node
-                currentNode = this.path[UnityEngine.Random.Range(0, this.path.Count)];
-            }
-            else
-            {
-                // Select newest node
-                currentNode = path[path.Count - 1];
-            }
+            MazeNode currentNode = ChooseActiveNode();
 
             // Check every neighbor of node
             List<Vector2> randomizedDirs = ShuffleList(DIRECTIONS);
-            MazeNode unvisitedNeighbor = null;
-
-            for (int i = 0; i < randomizedDirs.Count; i++)
-            {
-                Vector2 neighborPos = currentNode.mazePos + randomizedDirs[i];
-                
-                // Not neighbor if hit maze wall
-                if (neighborPos.x < 0 || // Left
-                    neighborPos.x > mazeSize.x - 1|| // Right
-                    neighborPos.y < 0 || // Bot
-                    neighborPos.y > mazeSize.y - 1 // Top
-                    )
-                {
-                    continue;
-                }
-                // arr to grid
-                // xcol = pos % width
-                // y = pos \ width
-
-                // grid to arr
-                // (y_row * row length) + xcol
-                
-                int neighborIdx = (int)((neighborPos.x * mazeSize.y) + neighborPos.y);
-                MazeNode neighbor = nodes[neighborIdx];
-
-                // Valid unvisited neighbor
-                if (!neighbor.isVisited)
-                {
-                    // Create path from currentnode to neighbor
-                    // Add to path
-                    // indicate new neighbor found
-                    // break from loop
-                    unvisitedNeighbor = neighbor;
-                    
-                    int dirkey = getDirKey(randomizedDirs[i]);
-                    CreatePath(currentNode, neighbor, dirkey);
-                    AddNode(neighbor);
-
-                    break;
-                }
-                else {
-                    continue;
-                }
-
-            }
+            MazeNode unvisitedNeighbor = FindUnvisitedNode(currentNode, randomizedDirs);
 
             // if no unvisited neighbors found, delete from path list
             if (unvisitedNeighbor == null)
@@ -130,6 +74,8 @@ public class GrowingTreeMazeManager : MonoBehaviour
                 this.completedNodes.Add(currentNode);
                 SetCompletedNode(currentNode);
             }
+
+            currentSteps++;
         }
         else if (this.path.Count == 0 && !finishedMaze)
         {
@@ -138,7 +84,74 @@ public class GrowingTreeMazeManager : MonoBehaviour
         }
     }
 
+    private MazeNode ChooseActiveNode()
+    {
+        MazeNode currentNode;
 
+        switch(this.cellSelectMethod)
+        {
+            case ACTIVE_SELECTION_METHOD.RANDOM:
+                currentNode = this.path[UnityEngine.Random.Range(0, this.path.Count)];
+                break;
+            case ACTIVE_SELECTION_METHOD.NEWEST:
+                // Select newest node
+                currentNode = path[path.Count - 1];
+                break;
+            case ACTIVE_SELECTION_METHOD.OLDEST:
+                currentNode = path[0];
+                break;
+            default:
+                currentNode = this.path[UnityEngine.Random.Range(0, this.path.Count)];
+                break;
+        }
+
+        return currentNode;
+    }
+
+    private MazeNode FindUnvisitedNode(MazeNode currentNode, List<Vector2> randomizedDirs)
+    {
+        MazeNode unvisitedNeighbor = null;
+
+        for (int i = 0; i < randomizedDirs.Count; i++)
+        {
+            Vector2 neighborPos = currentNode.mazePos + randomizedDirs[i];
+            
+            // Not neighbor if hit maze wall
+            if (neighborPos.x < 0 || neighborPos.x > mazeSize.x - 1 || // Left, Right
+                neighborPos.y < 0 || neighborPos.y > mazeSize.y - 1 // Bot, Top
+                )
+            {
+                continue;
+            }
+            // arr to grid
+            // xcol = pos % width
+            // y = pos \ width
+
+            // grid to arr
+            // (y_row * row length) + xcol
+            
+            int neighborIdx = (int)((neighborPos.x * mazeSize.y) + neighborPos.y);
+            MazeNode neighbor = nodes[neighborIdx];
+
+            // Valid unvisited neighbor
+            // Create path from currentnode to neighbor
+                // Add to path
+                // indicate new neighbor found
+                // break from loop
+            if (!neighbor.isVisited)
+            {
+                unvisitedNeighbor = neighbor;
+                
+                int dirkey = getDirKey(randomizedDirs[i]);
+                CreatePath(currentNode, neighbor, dirkey);
+                AddNode(neighbor);
+
+                break;
+            }
+        }
+
+        return unvisitedNeighbor;
+    }
 
 
     private List<MazeNode> GenerateGrid(Vector2Int mazeSize)
